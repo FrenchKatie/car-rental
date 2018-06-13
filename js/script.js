4113e4baec88567f91dee7fe6b9f0907d9a710a3
Katie elizabeth French <845855192@bun2-206-16.local>
2018-06-13 13:51:13 +1200

    working on page 3

--- js/script.js
+++ js/script.js
@@ -1,10 +1,32 @@
 $(document).ready(function() {
-			$('#pagepiling').pagepiling();
+			$('#pagepiling').pagepiling({
+					normalScrollElements: '#vehicleContent'
+			});
 
 			$( "#dateLeave" ).datepicker();
 			$( "#dateReturn" ).datepicker();
-			
+
+			// $.fn.pagepiling.setAllowScrolling(false);
+			//
+			// $( "#bla" ).click(function() {
+			//   	$.fn.pagepiling.moveSectionDown();
+			// });
 
 
-    // });
+			// $('#bikeInfoBtn').click(function(){
+	    //   $("#bikeInfo").slideToggle();
+			// });
+			$('#bikeInfoBtn').click(function(){
+	      toggleItemInfo();
+			});
+
+			// $('#smlCarInfoBtn').click(function(){
+	    //   $("#smlCarInfo").slideToggle();
+			// });
+
+
+			function toggleItemInfo(event){
+						event.target.slideToggle();
+			}
+
 });
