259af486fbde9a9278e97fdd8835104af9c0c948
Katie elizabeth French <845855192@bun2-206-16.local>
2018-06-15 11:50:51 +1200

    working on page 2, 3 and four

--- js/script.js
+++ js/script.js
@@ -24,6 +24,19 @@
 			 })
 
 
+			mapboxgl.accessToken = 'pk.eyJ1Ijoia2F0aWVmcmVuY2giLCJhIjoiY2ppM240MGFyMDA5cTNrbDJwNTkwYWZmOSJ9.vMh53hzHle4vA4uwg0TE6A';
+      var map = new mapboxgl.Map({
+                container: 'map',
+                style: 'mapbox://styles/mapbox/streets-v9',
+                center: [-79.4512, 43.6568],
+                zoom: 13
+      });
+
+      map.addControl(new MapboxDirections({
+                accessToken: mapboxgl.accessToken
+      }), 'top-left');
+
+
 
 
 
