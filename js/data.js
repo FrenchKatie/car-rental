89a8e44b9a0143c3191ed521a3a96e519ca0254f
Katie elizabeth French <845855192@bun2-206-16.local>
2018-06-19 15:54:11 +1200

    got date difference working - working on pickup/dropoff location

---
+++ js/data.js
@@ -0,0 +1,51 @@
+//-------------------------
+// Array of Objects (Vehicles)
+//-------------------------
+
+
+var vehicleData = [
+        motorbike = {
+              minSeats: 1,
+              maxSeats: 1,
+              pricePerDay: 109,
+              minDays: 1,
+              maxDays: 5,
+              fuelKm: 3.7
+        },
+        smallCar = {
+              minSeats:1,
+              maxSeats:2,
+              pricePerDay: 129,
+              minDays: 1,
+              maxDays: 10,
+              fuelKm: 8.5
+        },
+        largeCar = {
+              minSeats:1,
+              maxSeats:5,
+              pricePerDay: 144,
+              minDays:2,
+              maxDays: 10,
+              fuelKm:9.7
+        },
+        motorhome = {
+              minSeats: 2,
+              maxSeats: 6,
+              pricePerDay: 200,
+              minDays: 2,
+              maxDays: 15,
+              fuelKm: 17
+        }
+]
+
+var locationData = [
+      auckland = {
+        coordinates: [174.763332, -36.848460]
+      },
+      wellington = {
+        coordinates: [174.776236, -41.286460]
+      },
+      queenstown = {
+        coordinates: [168.662644, -45.031162]
+      }
+]
--- js/script.js
+++ js/script.js
@@ -26,28 +26,41 @@
 			//Toggling Item Info Dynamically
 			//------------------------------
 			$(".moreInfoBtn").click(function(){
-					 	console.dir(this);
+					 	// console.dir(this);
 				 	 	$(this).next().slideToggle();
 			})
-
 
 
 
 			//-------
 			//Map Box
  			//-------
+
+			// function createMap () {
+			// 	//get input fields
+			// 	var pickupLocation = document.getElementById('inputGroupSelect1');
+			// 	var dropoffLocation = document.getElementById('inputGroupSelect2');
+			// 	//get user input locations
+			// 	var getUserPickupLocation = pickupLocation.value;
+			// 	var getUserDropoffLocation = dropoffLocation.value;
+			// 	//Get drop off same as pickup
+			// 	//----if statement where getUserPickupLocation and getUserDropoffLocation will = eachother
+			// 	//Get coordinates
+			//
+			//
+			//
+			// }
+
+
+
 			mapboxgl.accessToken = 'pk.eyJ1Ijoia2F0aWVmcmVuY2giLCJhIjoiY2ppM240MGFyMDA5cTNrbDJwNTkwYWZmOSJ9.vMh53hzHle4vA4uwg0TE6A';
 
-      var map = new mapboxgl.Map({
-                container: 'map',
-                style: 'mapbox://styles/mapbox/streets-v9',
-                center: [-79.4512, 43.6568],
-                zoom: 13
-      });
-
-      map.addControl(new MapboxDirections({
-                accessToken: mapboxgl.accessToken
-      }), 'top-left');
+			var map = new mapboxgl.Map({
+								container: 'map',
+								style: 'mapbox://styles/mapbox/streets-v9',
+								center: [173.839405, -41.181890],
+								zoom: 5
+			});
 
 			var directions = new MapboxDirections({
 								accessToken:
@@ -56,84 +69,127 @@
 								profile: 'mapbox/driving'
 			});
 
-			directions.on('route', function(directions) {
-								console.dir(directions.route[0].distance / 1000 + 'kms');
-								console.dir(locationInputOne.value);
-								console.dir(locationInputTwo.value);
+			map.on('load', function() {
+			  				getRoute();
 			});
+
+			function getRoute() {
+							  var start = [174.475047, -37.021158];
+							  var end = [168.572909, -45.12498];
+							  var directionsRequest = 'https://api.mapbox.com/directions/v5/mapbox/driving/' + start[0] + ',' + start[1] + ';' + end[0] + ',' + end[1] + '?geometries=geojson&access_token=' + mapboxgl.accessToken;
+							  $.ajax({
+									    method: 'GET',
+									    url: directionsRequest,
+							  }).done(function(data) {
+									    var route = data.routes[0].geometry;
+									    map.addLayer({
+										      id: 'route',
+										      type: 'line',
+										      source: {
+												        type: 'geojson',
+												        data: {
+														          type: 'Feature',
+														          geometry: route
+												        }
+										      },
+										      paint: {
+								        				'line-width': 2
+										      }
+								    	}	);
+											map.addLayer({
+												  id: 'start',
+												  type: 'circle',
+												  source: {
+													    type: 'geojson',
+													    data: {
+														      type: 'Feature',
+														      geometry: {
+															        type: 'Point',
+															        coordinates: start
+														      }
+													    }
+												  }
+												});
+												map.addLayer({
+													  id: 'end',
+													  type: 'circle',
+													  source: {
+														    type: 'geojson',
+														    data: {
+															      type: 'Feature',
+															      geometry: {
+																        type: 'Point',
+																        coordinates: end
+															      }
+														    }
+													  }
+												});
+							  });
+			}
+
+
+
 
 
 // *** LOGIC JS ***
 
 
-			//-------------------------
-			// Array of Objects (Vehicles)
-			//-------------------------
+
+			//--------------------------------------
+			// Getting user input of Dates & Seats
+			//--------------------------------------
+			var leaveDate = document.getElementById('dateLeave');
+			var returnDate = document.getElementById('dateReturn');
+			var seatNumber = document.getElementById('seats');
 
 
-			var vehicles = [
-							motorbike = {
-										minSeats: 1,
-										maxSeats: 1,
-										pricePerDay: 109,
-										minDays: 1,
-										maxDays: 5,
-										fuelKm: 3.7
-							},
-							smallCar = {
-										minSeats:1,
-										maxSeats:2,
-										pricePerDay: 129,
-										minDays: 1,
-										maxDays: 10,
-										fuelKm: 8.5
-							},
-							largeCar = {
-										minSeats:1,
-										maxSeats:5,
-										pricePerDay: 144,
-										minDays:2,
-										maxDays: 10,
-										fuelKm:9.7
-							},
-							motorhome = {
-										minSeats: 2,
-										maxSeats: 6,
-										pricePerDay: 200,
-										minDays: 2,
-										maxDays: 15,
-										fuelKm: 17
-							}
-			]
+
+			$("#sectionOneSubmitBtn").click(function(){
+				//Getting the difference between the two dates
+						var date1 = new Date(leaveDate.value);
+						var date2 = new Date(returnDate.value);
+						var diffDays = parseInt((date2 - date1) / (1000 * 60 * 60 * 24));
+						// alert(diffDays)
+						console.log(diffDays);
+						console.log(seatNumber);
 
 
-			$(".submitBtn").click(function(){
-						console.dir(pickupDate.value);
-						console.dir(dropoffDate.value);
-						console.dir(seats.value);
+
 			})
 
-
-			var pickupDate = document.getElementById('dateLeave');
-			var dropoffDate = document.getElementById('dateReturn');
-			var seats = document.getElementById('seats');
-			var locationInputOne = document.getElementById('map').children[2].children["0"].children["0"].childNodes["0"].children["0"].children["0"].childNodes[1].childNodes[3].childNodes["0"].children[1];
-			var locationInputTwo = document.getElementById("map").childNodes[2].children["0"].children["0"].children["0"].children["0"].children["0"].children[2].children[1].children["0"].children[1];
-			var defaultTransportTypeInput = document.getElementById('map').childNodes[2].children["0"].children["0"].children["0"].children["0"].children[1];
-
-			defaultTransportTypeInput.style.display = "none";
-
-
-
-			// var date1 = new Date("7/13/2010");
-			// var date2 = new Date("12/15/2010");
-			// var timeDiff = date2.getTime() - date1.getTime();
-			// var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
-			// alert(diffDays);
+			//----------------------------------------------
+			// Showing vehicles that match date & seat needs
+			//----------------------------------------------
 
 
 
 
 
+
+			//--------------------------------------
+			// Mapping Route & Getting User location
+			//--------------------------------------
+
+			var pickupLocation = document.getElementById('inputGroupSelect1');
+			var dropoffLocation = document.getElementById('inputGroupSelect2');
+			var getUserPickupLocation = pickupLocation.value;
+			var getUserDropoffLocation = dropoffLocation.value;
+			//
+			$("#test").click(function(){
+						console.dir(pickupLocation.value);
+						// console.dir(dropoffLocation.value);
+
+						if (pickupLocation.value = auckland) {
+								alert("auckland yeah yeah");
+								console.dir(locationData["0"].coordinates[0]);
+						}else if(pickupLocation.value = wellington){
+							alert("welly ok ok ok");
+							console.log(locationData["1"].coordinates[0]);
+						}else if(pickupLocation.value = queenstown){
+							alert("queenstown wowie wowie");
+							console.log(locationData["2"].coordinates[0]);
+						}
+
+			})
 
 }());
